9f69a731d147f7cafe4666af6747d9bce4f7d41e
beckyGuev <73083696+beckyGuev@users.noreply.github.com>
2020-10-18 22:01:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<form method="POST" action="miFormulario.php">
+  Nombre: <input type="text" name="nombre">
+  Apellido: <input type="text" name="apellido">
+  Edad: <input type="text" name="edad">
+  Celular <input type="text" name="celular">
+  <input type="submit" name="enviar" value="crear">
+</form>
+
+
+
+</body>
+</html>
